5802431b2ee3d40448b23f1bd5e64e8003246b0e
Joseph Levy <jlevy@Josephs-MacBook-Pro-3.local>
2016-10-13 18:39:14 -0700

    commit2

---
+++ assets/games.js
@@ -0,0 +1,22 @@
+var wins = 0;
+var losses = 0;
+var word1before = ["_","_","_","_","_","_","_"];
+var word1 = ['h','a','t','c','h','e','t'];
+var arr== userGuess;
+var brr== word1;
+
+document.onkeyup = function(event){
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();}
+
+function wordGuess(arr,brr){
+	for (var i=0; i<arr.length && i<brr.length; i++) { 
+		if (arr.length[i]==brr.length[i])}
+		{console.log(userGuess);}
+} 
+
+
+
+console.log(h);
+console.log(arr);
+
+	
